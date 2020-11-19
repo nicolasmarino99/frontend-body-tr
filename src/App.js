e8e6f189ae6e6a533ea2117839d23caeadfe2ef2
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2020-11-19 14:41:41 -0500

    create states, api call and useeffect for handling logins

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import './App.css';
-import React, { Component } from 'react';
 import axios from 'axios'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-      isLoggedIn: false,
-      user: {}
-     };
-  };
-render() {
-    return (
-      <div>
-         <BrowserRouter>
-          <Switch>
-            <Route exact path='/' component={}/>
-            <Route exact path='/login' component={}/>
-            <Route exact path='/signup' component={}/>
-          </Switch>
-        </BrowserRouter>
-      </div>
-    );
+
+const App = props => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [user, setUser] = useState({})
+ 
+  const handleLogin = data => {
+    setIsLoggedIn(true)
+    setUser(data.user)
   }
+  const handleLogout = () => {
+    setIsLoggedIn(false)
+    setUser({})
+  }
+  useEffect(() => (
+    sendGetRequest()
+  ), [0])
+  const sendGetRequest = async () => {
+    try {
+        const response = await axios.get('http://localhost:3001/api/v1/logged_in', 
+        {withCredentials: true});
+        if (response.data.logged_in) {
+          handleLogin(response)
+        } else {
+          handleLogout()
+        }
+    } catch (err) {
+        // Handle Error Here
+        console.error(err);
+    }
+};
+  
+  return (
+    <div>
+        <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={}/>
+          <Route exact path='api/v1/login' component={}/>
+          <Route exact path='api/v1/signup' component={}/>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+  
 };
 export default App;
