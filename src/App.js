0c1f1e53f52efbbe3fd4ea483b6965e8af3811f9
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2020-11-19 14:15:51 -0500

    add react route and axios dependencies; add routing to app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+import React, { Component } from 'react';
+import axios from 'axios'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      isLoggedIn: false,
+      user: {}
+     };
+  };
+render() {
+    return (
+      <div>
+         <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={}/>
+            <Route exact path='/login' component={}/>
+            <Route exact path='/signup' component={}/>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
+};
 export default App;
